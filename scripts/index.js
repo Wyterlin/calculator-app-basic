9ec1b2cf2c23d3293663dfc9f636cc2c1c7ae92a
ChristianW$ <christianwyterlin@gmail.com>
2024-07-09 16:31:24 -0300

    Addition of operations

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,32 @@
+// Menção dos números
+
 const numbers = document.querySelectorAll('.number')
 
+// Menção dos telas
+
 const maindisplay = document.querySelector('.display__main')
+
+const historydisplay = document.querySelector('.display__history')
+
+// Menção das funções
 
 const dot = document.querySelector('.dot')
 
 const clean = document.querySelector('.clean')
 
 const backspace = document.querySelector('.action__clear-display')
+
+// Menção das operações
+
+const soma = document.querySelector('.soma')
+
+const sub = document.querySelector('.sub')
+
+const mult = document.querySelector('.mult')
+
+const div = document.querySelector('.div')
+
+const equal = document.querySelector('.equal')
 
 // Números da calculadora
 
@@ -34,6 +54,7 @@ dot.addEventListener("click", (event) => {
 
 clean.addEventListener("click", (event) => {
     maindisplay.innerText = 0
+    historydisplay.innerText = " "
 })
 
 // Função apagar
@@ -44,4 +65,102 @@ backspace.addEventListener("click", (event) => {
 
     if (maindisplay.innerText.length == 0)
         maindisplay.innerText = vazio
+})
+
+// Função soma
+
+soma.addEventListener("click", (event) => {
+    const value = event.target.innerText;
+    const result = Number(historydisplay.innerText) + Number(maindisplay.innerText)
+    const historyText = historydisplay.innerText.trim()
+
+    if (maindisplay && historydisplay) {
+        if (maindisplay.innerText !== "0") {
+            historydisplay.innerText = maindisplay.innerText;
+            maindisplay.innerText = 0
+
+        if (historyText !== "")
+            historydisplay.innerText = result
+        }
+    }
+})
+
+// Função subtração
+
+sub.addEventListener("click", (event) => {
+    const value = event.target.innerText;
+    const result = Number(historydisplay.innerText) - Number(maindisplay.innerText)
+    const historyText = historydisplay.innerText.trim()
+
+    if (maindisplay && historydisplay) {
+        if (maindisplay.innerText !== "0") {
+            historydisplay.innerText = maindisplay.innerText;
+            maindisplay.innerText = 0
+
+        if (historyText !== "")
+            historydisplay.innerText = result
+        }
+    }
+})
+
+// Função mult
+
+mult.addEventListener("click", (event) => {
+    const value = event.target.innerText;
+    const result = Number(historydisplay.innerText) * Number(maindisplay.innerText)
+    const historyText = historydisplay.innerText.trim()
+
+    if (maindisplay && historydisplay) {
+        if (maindisplay.innerText !== "0") {
+            historydisplay.innerText = maindisplay.innerText;
+            maindisplay.innerText = 0
+
+        if (historyText !== "")
+            historydisplay.innerText = result
+        }
+    }
+})
+
+div.addEventListener("click", (event) => {
+    const value = event.target.innerText;
+    const result = Number(historydisplay.innerText) / Number(maindisplay.innerText)
+    const historyText = historydisplay.innerText.trim()
+
+    if (maindisplay && historydisplay) {
+        if (maindisplay.innerText !== "0") {
+            historydisplay.innerText = maindisplay.innerText;
+            maindisplay.innerText = 0
+
+        if (historyText !== "")
+            historydisplay.innerText = result
+        }
+    }
+})
+
+equal.addEventListener("click", (event) => {
+    // const value = event.target.innerText;
+    const equal_soma = Number(maindisplay.innerText) + Number(maindisplay.innerText)
+    const equal_sub = Number(historydisplay.innerText) - Number(maindisplay.innerText)
+    const equal_mult = Number(historydisplay.innerText) * Number(maindisplay.innerText)
+    const equal_div = Number(historydisplay.innerText) / Number(maindisplay.innerText)
+    const historyText = historydisplay.innerText.trim()
+
+    if (maindisplay && historydisplay) {
+        if (maindisplay.innerText !== "0") {
+            historydisplay.innerText = maindisplay.innerText;
+            maindisplay.innerText = 0
+
+         if (historyText !== "" && soma)
+            historydisplay.innerText = equal_soma
+
+        if (historyText !== "" && sub)
+            historydisplay.innerText = equal_sub
+
+        if (historyText !== "" || mult)
+            historydisplay.innerText = equal_mult
+
+        if (historyText !== "" || div)
+            historydisplay.innerText = equal_div
+        }
+    }
 })
