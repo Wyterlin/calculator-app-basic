b5243ec332c575554c5936bbb18093b2c908f598
ChristianW$ <christianwyterlin@gmail.com>
2024-07-10 17:55:30 -0300

    versão00

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,14 @@
-// Menção dos números
+// Numbers
 
 const numbers = document.querySelectorAll('.number')
 
-// Menção dos telas
+// Operators
+
+const operador = document.querySelectorAll('.operator')
+
+const sec_operador = document.querySelectorAll('.sec_coperator')
+
+// Windows
 
 const maindisplay = document.querySelector('.display__main')
 
@@ -18,19 +24,12 @@ const clean = document.querySelector('.clean')
 
 const backspace = document.querySelector('.action__clear-display')
 
-// Menção das operações
-
-const soma = document.querySelector('.soma')
-
-const sub = document.querySelector('.sub')
-
-const mult = document.querySelector('.mult')
-
-const div = document.querySelector('.div')
+// Operator egual
 
 const equal = document.querySelector('.equal')
 
-// Números da calculadora
+
+// Calculator number
 
 numbers.forEach(function (el) {
     el.addEventListener("click", (event) => {
@@ -44,8 +43,32 @@ numbers.forEach(function (el) {
     })
 })
 
+// Calculator operator
 
-// Função Ponto
+operador.forEach(function (el) {
+    el.addEventListener("click", (event) => {
+        const operator = event.target.innerText
+
+        maindisplay.innerText += operator
+    })
+})
+
+// Calculator secundary operator
+
+sec_operador.forEach(function (el) {
+    el.addEventListener("click", (event) => {
+        const secoperator = event.target.innerText
+
+        if (secoperator = C) {
+            maindisplay.innerText = 0
+            historydisplay.innerText = " "
+            actionresult.innerText = 0
+        }
+
+    })
+})
+
+// Função dot
 
 dot.addEventListener("click", (event) => {
     const value = event.target.innerText
@@ -53,15 +76,15 @@ dot.addEventListener("click", (event) => {
     maindisplay.innerText += value
 })
 
-// Função Limpar
+// Função clear
 
-clean.addEventListener("click", (event) => {
-    maindisplay.innerText = 0
-    historydisplay.innerText = " "
-    actionresult.innerText = 0
-})
+// clean.addEventListener("click", (event) => {
+//     maindisplay.innerText = 0
+//     historydisplay.innerText = " "
+//     actionresult.innerText = 0
+// })
 
-// Função apagar
+// Função backspace
 
 backspace.addEventListener("click", (event) => {
     const vazio = 0
@@ -73,96 +96,98 @@ backspace.addEventListener("click", (event) => {
 
 // Função soma
 
-soma.addEventListener("click", (event) => {
-    const value = event.target.innerText;
-    const result = Number(historydisplay.innerText) + Number(maindisplay.innerText)
-    const historyText = historydisplay.innerText.trim()
+// soma.addEventListener("click", (event) => {
+//     const value = event.target.innerText;
+//     const result = Number(historydisplay.innerText) + Number(maindisplay.innerText)
+//     const historyText = historydisplay.innerText.trim()
 
-    if (maindisplay && historydisplay) {
-        if (maindisplay.innerText !== "0") {
-            historydisplay.innerText = maindisplay.innerText;
-            maindisplay.innerText = 0
+//     if (maindisplay && historydisplay) {
+//         if (maindisplay.innerText !== "0") {
+//             historydisplay.innerText = maindisplay.innerText;
+//             maindisplay.innerText = 0
 
-        if (historyText !== "")
-            historydisplay.innerText = result
-            actionresult.innerText = result
-        }
-    }
-})
+//         if (historyText !== "")
+//             historydisplay.innerText = result
+//             actionresult.innerText = result
+//         }
+//     }
+// })
 
 // Função subtração
 
-sub.addEventListener("click", (event) => {
-    const value = event.target.innerText;
-    const result = Number(historydisplay.innerText) - Number(maindisplay.innerText)
-    const historyText = historydisplay.innerText.trim()
+// sub.addEventListener("click", (event) => {
+//     const value = event.target.innerText;
+//     const result = Number(historydisplay.innerText) - Number(maindisplay.innerText)
+//     const historyText = historydisplay.innerText.trim()
 
-    if (maindisplay && historydisplay) {
-        if (maindisplay.innerText !== "0") {
-            historydisplay.innerText = maindisplay.innerText;
-            maindisplay.innerText = 0
+//     if (maindisplay && historydisplay) {
+//         if (maindisplay.innerText !== "0") {
+//             historydisplay.innerText = maindisplay.innerText;
+//             maindisplay.innerText = 0
 
-        if (historyText !== "")
-            historydisplay.innerText = result
-            actionresult.innerText = result
-        }
-    }
-})
+//         if (historyText !== "")
+//             historydisplay.innerText = result
+//             actionresult.innerText = result
+//         }
+//     }
+// })
 
-// Função mult
+// // Função mult
 
-mult.addEventListener("click", (event) => {
-    const value = event.target.innerText;
-    const result = Number(historydisplay.innerText) * Number(maindisplay.innerText)
-    const historyText = historydisplay.innerText.trim()
+// mult.addEventListener("click", (event) => {
+//     const value = event.target.innerText;
+//     const result = Number(historydisplay.innerText) * Number(maindisplay.innerText)
+//     const historyText = historydisplay.innerText.trim()
 
-    if (maindisplay && historydisplay) {
-        if (maindisplay.innerText !== "0") {
-            historydisplay.innerText = maindisplay.innerText;
-            maindisplay.innerText = 0
+//     if (maindisplay && historydisplay) {
+//         if (maindisplay.innerText !== "0") {
+//             historydisplay.innerText = maindisplay.innerText;
+//             maindisplay.innerText = 0
 
-        if (historyText !== "")
-            historydisplay.innerText = result
-            actionresult.innerText = result
-        }
-    }
-})
+//         if (historyText !== "")
+//             historydisplay.innerText = result
+//             actionresult.innerText = result
+//         }
+//     }
+// })
 
-div.addEventListener("click", (event) => {
-    const value = event.target.innerText;
-    const result = Number(historydisplay.innerText) / Number(maindisplay.innerText)
-    const historyText = historydisplay.innerText.trim()
+// div.addEventListener("click", (event) => {
+//     const value = event.target.innerText;
+//     const result = Number(historydisplay.innerText) / Number(maindisplay.innerText)
+//     const historyText = historydisplay.innerText.trim()
 
-    if (maindisplay && historydisplay) {
-        if (maindisplay.innerText !== "0") {
-            historydisplay.innerText = maindisplay.innerText;
-            maindisplay.innerText = 0
+//     if (maindisplay && historydisplay) {
+//         if (maindisplay.innerText !== "0") {
+//             historydisplay.innerText = maindisplay.innerText;
+//             maindisplay.innerText = 0
 
-        if (historyText !== "")
-            historydisplay.innerText = result
-            actionresult.innerText = result
-        }
-    }
-})
+//         if (historyText !== "")
+//             historydisplay.innerText = result
+//             actionresult.innerText = result
+//         }
+//     }
+// })
 
-equal.addEventListener("click", (event) => {
-    const equal_soma = Number(historydisplay.innerText) + Number(maindisplay.innerText);
-    const equal_sub = Number(historydisplay.innerText) - Number(maindisplay.innerText);
-    const equal_mult = Number(historydisplay.innerText) * Number(maindisplay.innerText);
-    const equal_div = Number(historydisplay.innerText) / Number(maindisplay.innerText);
+// equal.addEventListener("click", (event) => {
+//     const calculator_maindisplay = maindisplay.innerTex.trim();
+//     const calculator_operador = calculator__operator
 
-    if (maindisplay && historydisplay) {
-        if (maindisplay.innerText !== "0") {
-            historydisplay.innerText = maindisplay.innerText;
-            maindisplay.innerText = 0
-        } if (soma) {
-            historydisplay.innerText = equal_soma
-        } if (sub) {
-            historydisplay.innerText = equal_sub
-        } if (mult) {
-            historydisplay.innerText = equal_mult
-        } if (div) {
-            historydisplay.innerText = equal_div
-        }
-    }   
-})
+//     const firstValue  = Number(historydisplay.innerText) + Number(maindisplay.innerText);
+//     const operador = Number(historydisplay.innerText) - Number(maindisplay.innerText);
+//     const secondValue  = Number(historydisplay.innerText) * Number(maindisplay.innerText);
+
+//     if (maindisplay && historydisplay) {
+//         if (maindisplay.innerText !== "0") {
+//             historydisplay.innerText = maindisplay.innerText;
+//             maindisplay.innerText = 0
+//         } if (soma) {
+//             historydisplay.innerText = equal_soma
+//         } if (sub) {
+//             historydisplay.innerText = equal_sub
+//         } if (mult) {
+//             historydisplay.innerText = equal_mult
+//         } if (div) {
+//             historydisplay.innerText = equal_div
+//         }
+//     }   
+// })
